449750bc18fa7316f11bd27f265d7e553f30a1a6
Moksej <58531286+TheMoksej@users.noreply.github.com>
2021-01-19 20:00:10 +0100

    version update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
   name = 'DEL.py',
   packages=['delpy'],
-  version = '0.0.1',
+  version = '0.1',
   license='MIT',
   description = 'API wrapper for discordextremelist',
   author = 'Moksej',
